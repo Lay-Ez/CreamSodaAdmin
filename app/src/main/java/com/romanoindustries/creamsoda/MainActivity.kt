577ef1e726bc9d6f391aa33e49c0691d503733ab
Roman <deep.depp@yandex.ru>
2020-06-18 15:38:17 +0300

    setup bottom navigation view with nav components

--- app/src/main/java/com/romanoindustries/creamsoda/MainActivity.kt
+++ app/src/main/java/com/romanoindustries/creamsoda/MainActivity.kt
@@ -2,6 +2,9 @@ package com.romanoindustries.creamsoda
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +13,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupNavigation()
+    }
+
+    private fun setupNavigation() {
+        val navController = findNavController(R.id.nav_host_fragment)
+        bottom_nav_view_main.setupWithNavController(navController)
     }
 }
 
---
+++ app/src/main/java/com/romanoindustries/creamsoda/hookahsection/HookahFragment.kt
@@ -0,0 +1,19 @@
+package com.romanoindustries.creamsoda.hookahsection
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.romanoindustries.creamsoda.R
+
+class HookahFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_hookah, container, false)
+    }
+}
