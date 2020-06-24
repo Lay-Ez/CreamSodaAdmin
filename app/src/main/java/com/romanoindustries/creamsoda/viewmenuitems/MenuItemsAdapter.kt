6ce4dbdc2039987f9e5793781f5c0408af836c0c
Roman <deep.depp@yandex.ru>
2020-06-24 17:51:53 +0300

    Display menu items

---
+++ app/src/main/java/com/romanoindustries/creamsoda/viewmenuitems/MenuItemsAdapter.kt
@@ -0,0 +1,111 @@
+package com.romanoindustries.creamsoda.viewmenuitems
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import com.romanoindustries.creamsoda.R
+import com.romanoindustries.creamsoda.datamodel.MenuItem
+import com.squareup.picasso.Picasso
+
+class MenuItemsAdapter(var menuItems: List<MenuItem>): RecyclerView.Adapter<MenuItemsAdapter.MenuItemViewHolder>() {
+
+    fun updateMenuItems(menuItems: List<MenuItem>) {
+        this.menuItems = menuItems
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MenuItemViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.menu_item_list_item, parent, false)
+        return MenuItemViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = menuItems.size
+
+    override fun onBindViewHolder(holder: MenuItemViewHolder, position: Int) {
+        holder.bind(menuItems[position])
+    }
+
+    class MenuItemViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+        private var imageView: ImageView = itemView.findViewById(R.id.image_view)
+        private var editBtn: ImageButton = itemView.findViewById(R.id.image_btn_edit)
+
+        private var cardViewTag1: CardView = itemView.findViewById(R.id.card_view_tag_1)
+        private var textViewTag1: TextView = itemView.findViewById(R.id.text_view_tag_1)
+
+        private var cardViewTag2: CardView = itemView.findViewById(R.id.card_view_tag_2)
+        private var textViewTag2: TextView = itemView.findViewById(R.id.text_view_tag_2)
+
+        private var cardViewTag3: CardView = itemView.findViewById(R.id.card_view_tag_3)
+        private var textViewTag3: TextView = itemView.findViewById(R.id.text_view_tag_3)
+
+        private var textViewName: TextView = itemView.findViewById(R.id.text_view_name)
+        private var textViewDescription: TextView = itemView.findViewById(R.id.text_view_description)
+        private var textViewIngredients: TextView = itemView.findViewById(R.id.text_view_ingredients)
+        private var textViewWeight: TextView = itemView.findViewById(R.id.text_view_weight)
+        private var textViewWeightLabel: TextView = itemView.findViewById(R.id.text_view_weight_label)
+        private var textViewPrice: TextView = itemView.findViewById(R.id.text_view_price)
+
+        fun bind(menuItem: MenuItem) {
+            hideAllTags()
+            Picasso.get().load(menuItem.imageUrl).into(imageView)
+            textViewName.text = menuItem.name
+            textViewDescription.text = menuItem.description
+            textViewIngredients.text = menuItem.ingredients
+            textViewWeight.text = menuItem.weight.toString()
+            textViewPrice.text = menuItem.price.toString()
+            displayTags(menuItem.tags)
+        }
+
+        private fun displayTags(tags: List<String>) {
+            for (i in tags.indices) {
+                when (i) {
+                    0 -> {
+                        cardViewTag1.visibility = View.VISIBLE
+                        textViewTag1.text = tags[i].trim()
+                    }
+                    1 -> {
+                        cardViewTag2.visibility = View.VISIBLE
+                        textViewTag2.text = tags[i].trim()
+                    }
+                    2 -> {
+                        cardViewTag3.visibility = View.VISIBLE
+                        textViewTag3.text = tags[i].trim()
+                    }
+                }
+            }
+        }
+
+        private fun hideAllTags() {
+            cardViewTag1.visibility = View.INVISIBLE
+            cardViewTag2.visibility = View.INVISIBLE
+            cardViewTag3.visibility = View.INVISIBLE
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/romanoindustries/creamsoda/viewmenuitems/ViewMenuItemsFragment.kt
+++ app/src/main/java/com/romanoindustries/creamsoda/viewmenuitems/ViewMenuItemsFragment.kt
@@ -2,12 +2,14 @@ package com.romanoindustries.creamsoda.viewmenuitems
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.gson.Gson
 import com.romanoindustries.creamsoda.MyApp
 import com.romanoindustries.creamsoda.R
@@ -17,9 +19,6 @@ import com.romanoindustries.creamsoda.editcategory.CATEGORY_OBJECT_KEY
 import com.romanoindustries.creamsoda.newcategory.CATEGORY_FOOD
 import com.romanoindustries.creamsoda.newcategory.CATEGORY_TYPE_KEY
 import com.romanoindustries.creamsoda.newmenuitem.NewMenuItemActivity
-import kotlinx.android.synthetic.main.fragment_food.*
-import kotlinx.android.synthetic.main.fragment_food.floating_action_button
-import kotlinx.android.synthetic.main.fragment_food.toolbar
 import kotlinx.android.synthetic.main.fragment_view_menu_items.*
 
 class ViewMenuItemsFragment : Fragment() {
@@ -28,6 +27,7 @@ class ViewMenuItemsFragment : Fragment() {
     private lateinit var viewModel: ViewMenuItemsViewModel
     private lateinit var menuCategory: MenuCategory
     private lateinit var categoryType: String
+    private lateinit var adapter: MenuItemsAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_view_menu_items, container, false)
@@ -35,9 +35,16 @@ class ViewMenuItemsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView()
         arguments?.let { bundle ->
             parseBundle(bundle)
         }
+    }
+
+    private fun setupRecyclerView() {
+        adapter = MenuItemsAdapter(arrayListOf())
+        recycler_view.adapter = adapter
+        recycler_view.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
     }
 
     private fun displayMenuCategory(menuCategory: MenuCategory) {
@@ -73,7 +80,10 @@ class ViewMenuItemsFragment : Fragment() {
     }
 
     private fun displayMenuItems(menuItems: List<MenuItem>) {
-
+        menuItems.forEach {
+            Log.d(TAG, "displayMenuItems: $it")
+        }
+        adapter.updateMenuItems(menuItems)
     }
 }
 
