626c17eceab6fe511544c54265baadb353ee48c8
Roman <deep.depp@yandex.ru>
2020-06-28 14:14:08 +0300

    Implement activity to edit menu items

--- app/src/main/java/com/romanoindustries/creamsoda/editmenuitem/EditMenuItemActivity.kt
+++ app/src/main/java/com/romanoindustries/creamsoda/editmenuitem/EditMenuItemActivity.kt
@@ -1,5 +1,6 @@
 package com.romanoindustries.creamsoda.editmenuitem
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -13,11 +14,38 @@ import com.romanoindustries.creamsoda.R
 import com.romanoindustries.creamsoda.datamodel.MenuCategory
 import com.romanoindustries.creamsoda.datamodel.MenuItem
 import com.romanoindustries.creamsoda.editcategory.CATEGORY_OBJECT_KEY
+import com.romanoindustries.creamsoda.helpers.textChanges
+import com.romanoindustries.creamsoda.helpers.trimmedText
 import com.romanoindustries.creamsoda.newcategory.*
 import com.squareup.picasso.Picasso
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 import kotlinx.android.synthetic.main.activity_edit_menu_item.*
+import kotlinx.android.synthetic.main.activity_edit_menu_item.toolbar
 import kotlinx.android.synthetic.main.activity_edit_menu_item_inner.*
+import kotlinx.android.synthetic.main.activity_edit_menu_item_inner.card_view_tag_1
+import kotlinx.android.synthetic.main.activity_edit_menu_item_inner.card_view_tag_2
+import kotlinx.android.synthetic.main.activity_edit_menu_item_inner.card_view_tag_3
+import kotlinx.android.synthetic.main.activity_edit_menu_item_inner.edit_text_description
+import kotlinx.android.synthetic.main.activity_edit_menu_item_inner.edit_text_ingredients
+import kotlinx.android.synthetic.main.activity_edit_menu_item_inner.edit_text_name
+import kotlinx.android.synthetic.main.activity_edit_menu_item_inner.edit_text_price
+import kotlinx.android.synthetic.main.activity_edit_menu_item_inner.edit_text_tags
+import kotlinx.android.synthetic.main.activity_edit_menu_item_inner.edit_text_weight
+import kotlinx.android.synthetic.main.activity_edit_menu_item_inner.image_button_add_photo
+import kotlinx.android.synthetic.main.activity_edit_menu_item_inner.image_view_preview
+import kotlinx.android.synthetic.main.activity_edit_menu_item_inner.progress_bar_save
+import kotlinx.android.synthetic.main.activity_edit_menu_item_inner.progress_bar_upload
+import kotlinx.android.synthetic.main.activity_edit_menu_item_inner.text_input_description
+import kotlinx.android.synthetic.main.activity_edit_menu_item_inner.text_input_ingredients
+import kotlinx.android.synthetic.main.activity_edit_menu_item_inner.text_input_name
+import kotlinx.android.synthetic.main.activity_edit_menu_item_inner.text_input_price
+import kotlinx.android.synthetic.main.activity_edit_menu_item_inner.text_input_weight
+import kotlinx.android.synthetic.main.activity_edit_menu_item_inner.text_view_tag_1
+import kotlinx.android.synthetic.main.activity_edit_menu_item_inner.text_view_tag_2
+import kotlinx.android.synthetic.main.activity_edit_menu_item_inner.text_view_tag_3
+import kotlinx.android.synthetic.main.activity_new_menu_item.*
+import kotlinx.android.synthetic.main.activity_new_menu_item_inner.*
+import java.lang.NumberFormatException
 
 const val MENU_ITEM_OBJECT_KEY = "menu_item"
 
@@ -29,8 +57,162 @@ class EditMenuItemActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_menu_item)
+        setupListeners()
         parseIntent()
         observeViewModel()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable.dispose()
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        viewModel.imageUrl.removeObservers(this)
+        viewModel.cancelImageUpload()
+        viewModel.deleteCurrentImage()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == PICK_IMAGE_REQUEST
+            && resultCode == Activity.RESULT_OK
+            && data != null
+            && data.data != null) {
+            val imageUri = data.data!!
+            viewModel.uploadImage(imageUri, contentResolver)
+        }
+    }
+
+    private fun onSavePressed() {
+        if (isInputCorrect()) {
+            loadInputToViewModel()
+        }
+    }
+
+    private fun loadInputToViewModel() {
+        val name = edit_text_name.trimmedText()
+        val description = edit_text_description.trimmedText()
+        val ingredients = edit_text_ingredients.trimmedText()
+        val weight = edit_text_weight.trimmedText().toInt()
+        val price = edit_text_price.trimmedText().toInt()
+        var tagsList = edit_text_tags.trimmedText().split(",")
+        tagsList.forEach { it.trim() }
+        if (tagsList.size > 3) {
+            tagsList = tagsList.subList(0, 3)
+        }
+        viewModel.saveItem(name, ingredients, description, price, weight, tagsList)
+    }
+
+    private fun isInputCorrect(): Boolean {
+        var inputCorrect = true
+        val name = edit_text_name.trimmedText()
+        val description = edit_text_description.trimmedText()
+        val ingredients = edit_text_ingredients.trimmedText()
+        val weightStr = edit_text_weight.trimmedText()
+        val priceStr = edit_text_price.trimmedText()
+
+        if (name.isBlank()) {
+            inputCorrect = false
+            text_input_name.error = getString(R.string.error_empty_field)
+        }
+        if (description.isBlank()) {
+            inputCorrect = false
+            text_input_description.error = getString(R.string.error_empty_field)
+        }
+        if (ingredients.isBlank()) {
+            inputCorrect = false
+            text_input_ingredients.error = getString(R.string.error_empty_field)
+        }
+
+        try {
+            val weight = weightStr.toInt()
+            if (weight <= 0) {
+                text_input_weight.error = getString(R.string.error_incorrect_format)
+                inputCorrect = false
+            }
+        } catch (e: NumberFormatException) {
+            text_input_weight.error = getString(R.string.error_incorrect_format)
+            inputCorrect = false
+        }
+
+        try {
+            val price = priceStr.toInt()
+            if (price <= 0) {
+                text_input_price.error = getString(R.string.error_incorrect_format)
+                inputCorrect = false
+            }
+        } catch (e: NumberFormatException) {
+            text_input_price.error = getString(R.string.error_incorrect_format)
+            inputCorrect = false
+        }
+
+        return inputCorrect
+    }
+
+    private fun setupListeners() {
+        toolbar.setNavigationOnClickListener { onBackPressed() }
+        toolbar.setOnMenuItemClickListener {menuItem ->
+            when (menuItem.itemId) {
+                R.id.mnu_item_save -> {
+                    onSavePressed()
+                    true
+                }
+                R.id.mnu_item_delete -> {
+                    onDeletePressed()
+                    true
+                }
+                else -> false
+            }
+        }
+
+        hideAllTags()
+        edit_text_tags.textChanges().subscribe {text ->
+            val tagsList = text.trim().split(",")
+            tagsList.forEach { it.trim() }
+            hideAllTags()
+            for (i in tagsList.indices) {
+                when (i) {
+                    0 -> {
+                        card_view_tag_3.visibility = View.VISIBLE
+                        text_view_tag_3.visibility = View.VISIBLE
+                        text_view_tag_3.text = tagsList[i]
+                    }
+                    1 -> {
+                        card_view_tag_2.visibility = View.VISIBLE
+                        text_view_tag_2.visibility = View.VISIBLE
+                        text_view_tag_2.text = tagsList[i]
+                    }
+                    2 -> {
+                        card_view_tag_1.visibility = View.VISIBLE
+                        text_view_tag_1.visibility = View.VISIBLE
+                        text_view_tag_1.text = tagsList[i]
+                    }
+                }
+            }
+        }.also { compositeDisposable.add(it) }
+
+        edit_text_name.textChanges()
+            .subscribe { text_input_name.error = null }.also { compositeDisposable.add(it) }
+        edit_text_description.textChanges()
+            .subscribe { text_input_description.error = null }.also { compositeDisposable.add(it) }
+        edit_text_ingredients.textChanges()
+            .subscribe { text_input_ingredients.error = null }.also { compositeDisposable.add(it) }
+        edit_text_weight.textChanges()
+            .subscribe { text_input_weight.error = null }.also { compositeDisposable.add(it) }
+        edit_text_price.textChanges()
+            .subscribe { text_input_price.error = null }.also { compositeDisposable.add(it) }
+    }
+
+    private fun onDeletePressed() {
+        viewModel.deleteItem()
+    }
+
+    private fun hideAllTags() {
+        card_view_tag_1.visibility = View.INVISIBLE
+        card_view_tag_2.visibility = View.INVISIBLE
+        card_view_tag_3.visibility = View.INVISIBLE
     }
 
     private fun parseIntent() {
--- app/src/main/java/com/romanoindustries/creamsoda/editmenuitem/EditMenuItemViewModel.kt
+++ app/src/main/java/com/romanoindustries/creamsoda/editmenuitem/EditMenuItemViewModel.kt
@@ -1,5 +1,8 @@
 package com.romanoindustries.creamsoda.editmenuitem
 
+import android.content.ContentResolver
+import android.net.Uri
+import android.webkit.MimeTypeMap
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -10,10 +13,10 @@ import com.romanoindustries.creamsoda.RepositoryComponent
 import com.romanoindustries.creamsoda.datamodel.MenuCategory
 import com.romanoindustries.creamsoda.datamodel.MenuItem
 import com.romanoindustries.creamsoda.menurepository.MenuRepository
-import com.romanoindustries.creamsoda.newcategory.CATEGORY_FOOD
-import com.romanoindustries.creamsoda.newcategory.STATE_DEFAULT
+import com.romanoindustries.creamsoda.newcategory.*
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.subjects.PublishSubject
+import java.lang.Exception
 
 class EditMenuItemViewModel: ViewModel() {
 
@@ -42,6 +45,97 @@ class EditMenuItemViewModel: ViewModel() {
     private var currentTask: StorageTask<UploadTask.TaskSnapshot>? = null
     private var imageName = ""
 
+    fun uploadImage(imageUri: Uri, contentResolver: ContentResolver) {
+        cancelImageUpload()
+        deleteCurrentImage()
+        isLoadingMutable.value = true
+        imageName = "${System.currentTimeMillis()}.${getFileExtension(imageUri, contentResolver)}"
+        currentTask = storageReference.child(imageName)
+            .putFile(imageUri)
+            .addOnSuccessListener {taskSnapshot ->
+                val taskUri = taskSnapshot.storage.downloadUrl
+                taskUri.addOnSuccessListener {uri ->
+                    currentTask?.let {
+                        if (it.isCanceled) {
+                            deleteCurrentImage()
+                        } else {
+                            imageUrlMutable.value = uri.toString()
+                        }
+                    }
+                }
+            }
+            .addOnFailureListener {
+                errorChannelSubject.onNext(ERROR_UPLOADING_IMAGE)
+            }
+            .addOnProgressListener {
+                val progress = (100.00 * it.bytesTransferred / it.totalByteCount)
+                uploadProgressMutable.value = progress.toInt()
+            }
+            .addOnCompleteListener{
+                isLoadingMutable.value = false
+            }
+    }
+
+    fun saveItem(name: String,
+                 ingredients: String,
+                 description: String,
+                 price: Int,
+                 weight: Int,
+                 tags: List<String>) {
+        val imageUrl = imageUrl.value
+        if (imageUrl == null || imageUrl.length < 5 || imageName.isEmpty()) {
+            errorChannelSubject.onNext(ERROR_IMAGE_NOT_LOADED)
+            return
+        }
+        stateMutable.value = STATE_SAVING
+        val initialImageName = initialMenuItem.imageName
+        if (initialImageName != imageName && initialImageName.isNotBlank()) {
+            storageReference.child(initialImageName).delete()
+        }
+        initialMenuItem.apply {
+            this.name = name
+            this.ingredients = ingredients
+            this.description = description
+            this.price = price
+            this.weight = weight
+            this.tags = tags
+            this.imageUrl = imageUrl
+        }
+        initialMenuItem.imageName = imageName
+        menuRepo.updateMenuItem(menuCategory, initialMenuItem)
+            .subscribe({
+                stateMutable.value = STATE_SAVED
+            }, {
+                errorChannelSubject.onNext(ERROR_SAVING_CATEGORY)
+                stateMutable.value = STATE_DEFAULT
+            })
+    }
+
+    fun deleteItem() {
+        stateMutable.value = STATE_SAVING
+        cancelImageUpload()
+        deleteCurrentImage()
+        storageReference.child(initialMenuItem.imageName)
+            .delete()
+            .addOnSuccessListener {
+                menuRepo.deleteMenuItem(menuCategory, initialMenuItem)
+                    .subscribe({
+                        stateMutable.value = STATE_SAVED
+                    }, {
+                        stateMutable.value = STATE_DEFAULT
+                        errorChannelSubject.onNext(ERROR_SAVING_CATEGORY)
+                    })
+            }
+            .addOnFailureListener {
+                stateMutable.value = STATE_DEFAULT
+                errorChannelSubject.onNext(ERROR_SAVING_CATEGORY)
+            }
+    }
+
+    private fun getFileExtension(uri: Uri, contentResolver: ContentResolver): String? {
+        val mime = MimeTypeMap.getSingleton()
+        return mime.getExtensionFromMimeType(contentResolver.getType(uri))
+    }
 
     fun setupValues(repositoryComponent: RepositoryComponent,
                     category: String, menuCategory: MenuCategory, menuItem: MenuItem) {
@@ -52,6 +146,8 @@ class EditMenuItemViewModel: ViewModel() {
         this.storageReference = repositoryComponent.getImagesStorageReference()
         this.menuCategory = menuCategory
         initialMenuItem = menuItem
+        imageName = initialMenuItem.imageName
+        imageUrlMutable.value = menuItem.imageUrl
     }
 
     fun cancelImageUpload() {
@@ -59,6 +155,14 @@ class EditMenuItemViewModel: ViewModel() {
         isLoadingMutable.value = false
         uploadProgressMutable.value = 0
         imageUrlMutable.value = "-" /* cannot set to empty - picasso will throw error*/
+    }
+
+    fun deleteCurrentImage() {
+        if (imageName != initialMenuItem.imageName) {
+            try {
+                storageReference.child(imageName).delete()
+            } catch (e: Exception) {}
+        }
     }
 
 }
